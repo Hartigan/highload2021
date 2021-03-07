920cb74f9e210fbb1fc24beb9efaa6e02219b953
Anatoly Safonov <hartigans@live.com>
2021-03-07 19:17:32 +0300

    Save digger

--- src/Miner/DiggerWorker.cs
+++ src/Miner/DiggerWorker.cs
@@ -24,135 +24,131 @@ namespace Miner
             _cells = cells;
         }
 
-        private async Task<License> GetLicenseAsync(Stack<int> myCoins, Client client, bool free)
+        private Task<License> GetLicenseAsync(ConcurrentBag<int> myCoins, Client client)
         {
             List<int> coins = _empty;
 
-            if (!free) {
-                if (myCoins.Count > 0)
+            if (myCoins.Count > 0)
+            {
+                coins = new List<int>();
+                // if (myCoins.Count > 21)
+                // {
+                //     for(int i = 0; i < 21; ++i)
+                //     {
+                //         int c = 0;
+                //         if (!myCoins.TryTake(out c))
+                //         {
+                //             _logger.LogDebug("buy license wtf 21");
+                //         }
+                //         coins.Add(c);
+                //     }
+                // }
+                // else if (myCoins.Count > 11)
+                // {
+                //     for(int i = 0; i < 11; ++i)
+                //     {
+                //         int c = 0;
+                //         if (!myCoins.TryTake(out c))
+                //         {
+                //             _logger.LogDebug("buy license wtf 11");
+                //         }
+                //         coins.Add(c);
+                //     }
+                // }
+                // else if (myCoins.Count > 6)
+                // {
+                //     for(int i = 0; i < 6; ++i)
+                //     {
+                //         int c = 0;
+                //         if (!myCoins.TryTake(out c))
+                //         {
+                //             _logger.LogDebug("buy license wtf 6");
+                //         }
+                //         coins.Add(c);
+                //     }
+                // }
+                // else
                 {
-                    coins = new List<int>();
-                    coins.Add(myCoins.Pop());
+                    int c = 0;
+                    if (!myCoins.TryTake(out c))
+                    {
+                        _logger.LogDebug("buy license wtf 1");
+                    }
+                    coins.Add(c);
                 }
             }
-
-            return await client.BuyLicenseAsync(coins);
+            return client.BuyLicenseAsync(coins);
         }
 
-        private async Task SellAsync(string treasure, Stack<int> myCoins, Client client)
+        private async Task SellAsync(string treasure, ConcurrentBag<int> myCoins, Client client)
         {
             List<int> coins = await client.CashAsync(treasure);
 
-            if (myCoins.Count > 100) {
+            if (myCoins.Count > 1000) {
                 return;
             }
 
             foreach(var coin in coins) {
-                myCoins.Push(coin);
+                myCoins.Add(coin);
             }
         }
 
         public async Task Doit()
         {
-            Stack<int> myCoins = new Stack<int>();
+            ConcurrentBag<int> myCoins = new ConcurrentBag<int>();
             Stack<Task> cashTasks = new Stack<Task>();
+
+            int[] counts = new int[10];
 
             Client licenseClient = _clientFactory.Create();
             Client cashClient = _clientFactory.Create();
+            Client client = _clientFactory.Create();
 
-            Client[] diggerClients = {
-                _clientFactory.Create(),
-                _clientFactory.Create(),
-                _clientFactory.Create(),
-                _clientFactory.Create(),
-                _clientFactory.Create(),
-            };
-
-            License license = null; 
             int waitCellCounter = 0;
 
             List<MyNode> currentNodes = new List<MyNode>(5);
             
             while(true) {
 
-                while (currentNodes.Count > 3)
-                {
-                    _cells.Enqueue(currentNodes[currentNodes.Count - 1]);
-                    currentNodes.RemoveAt(currentNodes.Count - 1);
-                }
+                License license = await GetLicenseAsync(myCoins, client);
 
-                while(currentNodes.Count < 3)
+                while(currentNodes.Count < license.DigAllowed)
                 {
                     MyNode node = null;
                     while(!_cells.TryDequeue(out node))
                     {
                         ++waitCellCounter;
                         if (waitCellCounter % 400000 == 0) {
-                            _logger.LogDebug($"Wait cell total count = {waitCellCounter}");
+                            //_logger.LogDebug($"Wait cell total count = {waitCellCounter}");
+                            //var c = String.Join(" ", counts);
+                            //_logger.LogDebug(c);
                         }
                         await Task.Yield();
                     }
                     currentNodes.Add(node);
                 }
 
+                var results = await Task.WhenAll(
+                    currentNodes
+                        .Take(license.DigAllowed)
+                        .Select(node => new Dig()
+                            {
+                                LicenseId = license.Id ?? 0,
+                                PosX = node.Report.Area.PosX,
+                                PosY = node.Report.Area.PosY,
+                                Depth = node.Depth
+                        })
+                        .Select(client.DigAsync));
+
+
+                for(int i = 0; i < results.Length; ++i)
                 {
-                    MyNode node = null;
-
-                    if (_cells.TryDequeue(out node))
-                    {
-                        currentNodes.Add(node);
-
-                        while(!_cells.TryDequeue(out node))
-                        {
-                            ++waitCellCounter;
-                            if (waitCellCounter % 400000 == 0) {
-                                _logger.LogDebug($"Wait cell total count = {waitCellCounter}");
-                            }
-                            await Task.Yield();
-                        }
-
-                        currentNodes.Add(node);
-                    }
-
+                    counts[currentNodes[i].Depth - 1] += results[i].Count;
+                    currentNodes[i].Depth++;
+                    currentNodes[i].Report.Amount -= results[i].Count;
                 }
 
-                switch(currentNodes.Count)
-                {
-                    case 5:
-                        license = await GetLicenseAsync(myCoins, licenseClient, false);
-                        break;
-                    case 3:
-                        license = await GetLicenseAsync(myCoins, licenseClient, true);
-                        break;
-                    default:
-                        _logger.LogCritical($"Stupid error");
-                        break;
-                }
-
-                while (currentNodes.Count > license.DigAllowed)
-                {
-                    _cells.Enqueue(currentNodes[currentNodes.Count - 1]);
-                    currentNodes.RemoveAt(currentNodes.Count - 1);
-                }
-
-
-                var results = await Task.WhenAll(currentNodes.Select(async (node, index) => {
-                    var dig = new Dig()
-                    {
-                        LicenseId = license.Id ?? 0,
-                        PosX = node.Report.Area.PosX,
-                        PosY = node.Report.Area.PosY,
-                        Depth = node.Depth
-                    };
-                    node.Depth++;
-                    var result = await diggerClients[index].DigAsync(dig);
-                    if (result != null) {
-                        node.Report.Amount -= result.Count;
-                    }
-                    return result;
-                }));
-
-                var treasures = results.SelectMany(x => x == null ? Enumerable.Empty<string>() : x);
+                var treasures = results.SelectMany(x => x);
 
                 currentNodes.RemoveAll(x => x.Depth > 10 || x.Report.Amount <= 0);
 
